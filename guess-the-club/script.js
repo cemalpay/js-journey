c543a82853d4a7e3e9d24bbb5b422bebada7f754
cemalpay <cem.alpay.tas@common.studio>
2022-09-25 20:31:04 +0300

    js settings

---
+++ guess-the-club/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//
+var clubsArray = [
+  "adanaspor",
+  "galatasaray",
+  "fenerbahce",
+  "besiktas",
+  "clermont",
+  "freiburg",
+  "goztepe",
+  "karabukspor",
+  "karsiyaka",
+  "lille",
+  "mainz",
+  "milan",
+  "rennes",
+  "shaktar",
+  "strasbourg",
+  "valencia",
+];
+let secretTeam = clubsArray[Math.floor(Math.random() * clubsArray.length)];
+let score = 20;
+let highscore = 0;
