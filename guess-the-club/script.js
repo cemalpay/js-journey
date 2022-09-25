7bf7c7b3d933895ab697c586a43c97d51f32885d
cemalpay <cem.alpay.tas@common.studio>
2022-09-25 21:41:57 +0300

    if/else

--- guess-the-club/script.js
+++ guess-the-club/script.js
@@ -20,5 +20,44 @@ var clubsArray = [
   "valencia",
 ];
 let secretTeam = clubsArray[Math.floor(Math.random() * clubsArray.length)];
+console.log(secretTeam);
 let score = 20;
 let highscore = 0;
+
+document.getElementById("club-img").src = `logos/${secretTeam}.jpg`;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = document.querySelector(".guess").value;
+
+  // input boş olduğu zaman
+  if (!guess) {
+    displayMessage("Make a guess!");
+
+    // doğru bilindiği zaman
+  } else if (guess === secretTeam) {
+    document.getElementById("club-img").src = "secretTeam";
+    displayMessage("Congrats!");
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    //highscore fonksiyonu
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").text = highscore;
+
+      // yanlış bilindiği zaman
+    } else if (guess !== secretTeam) {
+      if (score > 1) {
+        displayMessage("Wrong Answer!");
+        score--;
+        document.querySelector(".score").textContent = score;
+      } else {
+        displayMessage("Game over!");
+        document.querySelector(".score").textContent = 0;
+      }
+    }
+  }
+});
