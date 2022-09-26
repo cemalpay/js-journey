ff58402d1caf2cf9cc7fe4119d90a6cc18a750d8
cemalpay <cem.alpay.tas@common.studio>
2022-09-26 16:58:01 +0300

    prettier changes

--- guess-the-club/script.js
+++ guess-the-club/script.js
@@ -1,90 +1,90 @@
-"use strict";
+'use strict';
 
 //
 var clubsArray = [
-  "adanaspor",
-  "galatasaray",
-  "fenerbahce",
-  "besiktas",
-  "clermont",
-  "freiburg",
-  "goztepe",
-  "karabukspor",
-  "karsiyaka",
-  "lille",
-  "mainz",
-  "milan",
-  "rennes",
-  "shaktar",
-  "strasbourg",
-  "valencia",
+  'adanaspor',
+  'galatasaray',
+  'fenerbahce',
+  'besiktas',
+  'clermont',
+  'freiburg',
+  'goztepe',
+  'karabukspor',
+  'karsiyaka',
+  'lille',
+  'mainz',
+  'milan',
+  'rennes',
+  'shaktar',
+  'strasbourg',
+  'valencia',
   //------ NEW UPDATE -------------//
-  "almeria",
-  "altay",
-  "birmingham",
-  "bournemouth",
-  "bursaspor",
-  "everton",
-  "fiorentina",
-  "girona",
-  "hamburg",
-  "liverpool",
-  "newcastle",
-  "tottenham",
+  'almeria',
+  'altay',
+  'birmingham',
+  'bournemouth',
+  'bursaspor',
+  'everton',
+  'fiorentina',
+  'girona',
+  'hamburg',
+  'liverpool',
+  'newcastle',
+  'tottenham',
 ];
 let secretTeam = clubsArray[Math.floor(Math.random() * clubsArray.length)];
 console.log(secretTeam);
 let score = 20;
 let highscore = 0;
 
-document.getElementById("club-img").src = `logos/${secretTeam}.jpg`;
+document.getElementById('club-img').src = `logos/${secretTeam}.jpg`;
 
 const displayMessage = function (message) {
-  document.querySelector(".message").textContent = message;
+  document.querySelector('.message').textContent = message;
 };
 
-document.querySelector(".check").addEventListener("click", function () {
-  const guess = document.querySelector(".guess").value;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = document.querySelector('.guess').value;
 
   // input boş olduğu zaman
   if (!guess) {
-    displayMessage("Make a guess!");
+    displayMessage('Make a guess!');
 
     // doğru bilindiği zaman
   } else if (guess === secretTeam) {
-    document.getElementById("club-img").src = `logos/${secretTeam}.jpg`;
-    displayMessage("Congrats!");
-    document.querySelector("body").style.backgroundColor = "#60b347";
-    document.getElementById("club-img").style.filter = `blur(0px)`;
+    document.getElementById('club-img').src = `logos/${secretTeam}.jpg`;
+    displayMessage('Congrats!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.getElementById('club-img').style.filter = `blur(0px)`;
     //highscore fonksiyonu
     if (score > highscore) {
       highscore = score;
-      document.querySelector(".highscore").textContent = highscore;
+      document.querySelector('.highscore').textContent = highscore;
     }
 
     // yanlış bilindiği zaman
   } else if (guess !== secretTeam) {
     if (score > 1) {
-      displayMessage("Wrong Answer!");
+      displayMessage('Wrong Answer!');
       score--;
-      document.querySelector(".score").textContent = score;
-      document.getElementById("club-img").style.filter = `blur(3px)`;
+      document.querySelector('.score').textContent = score;
+      document.getElementById('club-img').style.filter = `blur(3px)`;
     } else {
-      displayMessage("Game over!");
-      document.querySelector(".score").textContent = 0;
+      displayMessage('Game over!');
+      document.querySelector('.score').textContent = 0;
     }
   }
 });
 
-document.querySelector(".again").addEventListener("click", function () {
+document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretTeam = clubsArray[Math.floor(Math.random() * clubsArray.length)];
   console.log(secretTeam);
-  displayMessage("Start guessing...");
-  document.querySelector(".score").textContent = score;
-  document.getElementById("club-img").src = `logos/${secretTeam}.jpg`;
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.getElementById('club-img').src = `logos/${secretTeam}.jpg`;
   // document.querySelector(".number").textContent = "?";
-  document.querySelector(".guess").value = "";
-  document.querySelector("body").style.backgroundColor = "#222";
-  document.getElementById("club-img").style.filter = `blur(5px) grayscale(1)`;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.getElementById('club-img').style.filter = `blur(5px) grayscale(1)`;
 });
---
+++ pop-up-experiment/script.js
@@ -0,0 +1,26 @@
+'use strict';
+// class'larını queryselector ile çağırıp variable tanımladık ki her defasında queryselector kullanmayalım!
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+// hidden class'ını kaldırıp pop-up'ı görünür yapan bir fonksiyon oluşturduk.
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// hidden class'ını ekleyip pop-up'ı görünmez yapan bir fonksiyon oluşturduk.
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// addEventListener kullanarak 3 butonun da tıklanabilir olmasını sağladık tek tek yapmak yerine.
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+// click edildiği zaman closeModal fonksiyonu çalışıyor.
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
