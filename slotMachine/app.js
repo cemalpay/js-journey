fefcf761a1701353a1ab57c152be96b57e7befa7
cemalpay <cem.alpay.tas@common.studio>
2022-10-06 17:56:00 +0300

    adding delay to lever

--- slotMachine/app.js
+++ slotMachine/app.js
@@ -1,11 +1,20 @@
 'use strict';
 
+function delay(milliseconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
 const balance = document.querySelector('.balance');
 
 balance.textContent = 100;
 
 const leverPressed = document.querySelector('.lever');
 
-leverPressed.addEventListener('click', function () {
+leverPressed.addEventListener('click', async function () {
   console.log('tiklaniyor.');
+  leverPressed.classList.add('lever-active');
+  await delay(1000);
+  leverPressed.classList.remove('lever-active');
 });
