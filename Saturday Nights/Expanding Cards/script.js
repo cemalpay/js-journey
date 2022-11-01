a8d626227e4c974e1f117e6c17146c4f017a0685
cemalpay <cem.alpay.tas@common.studio>
2022-11-01 18:17:55 +0300

    js

--- Saturday Nights/Expanding Cards/script.js
+++ Saturday Nights/Expanding Cards/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeAC();
+    panel.classList.add('active');
+  });
+});
+
+function removeAC() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
