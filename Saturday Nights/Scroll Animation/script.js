7f3af90eaf837c7056f5db4f957abf15c39455b3
cemalpay <cem.alpay.tas@common.studio>
2022-11-07 18:00:11 +0300

    Add comment to new functions

--- Saturday Nights/Scroll Animation/script.js
+++ Saturday Nights/Scroll Animation/script.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', checkBoxes)
 checkBoxes()
 function checkBoxes() {
     const triggerBottom = window.innerHeight / 5 * 4
-
+    //getBoundingClientReact box'umuzun en üst noktasının pozisyonunu söylüyor
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top
 
