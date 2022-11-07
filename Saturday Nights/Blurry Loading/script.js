3510d9aaf439d6eb9df9ef5fcc3ad57c2d6799e1
cemalpay <cem.alpay.tas@common.studio>
2022-11-07 16:08:42 +0300

    Update script.js

--- Saturday Nights/Blurry Loading/script.js
+++ Saturday Nights/Blurry Loading/script.js
@@ -16,7 +16,8 @@ function blurring() {
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-//
+/*bu fonksiyonu kullanarak sayı 0'dan 100'e yükselirken opacity'nin 1'den 0'a düşmesini sağladık
+blur'un da 30'dan 0'a düşmesini */
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; 
 }
