53eaebf1a68cca848a02a624f81513b76d33f57f
cemalpay <cem.alpay.tas@common.studio>
2022-11-10 16:35:59 +0300

    Call joke API & Print to HTML

--- Saturday Nights/Dad Jokes (using API)/script.js
+++ Saturday Nights/Dad Jokes (using API)/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke();
+
+//More clean
+async function generateJoke(){
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
+    
+};
+
+
+//Messy way
+// function generateJoke(){
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     };
+//     fetch('https://icanhazdadjoke.com', config).then(res => res.json().then(data => jokeEl.innerHTML = data.joke))
+// };
