c086cf881e4cad25bbf0a73678d970950478833c
cemalpay <cem.alpay.tas@common.studio>
2022-11-11 18:12:36 +0300

    Import font with tailwind.config.js
    https://daily-dev-tips.com/posts/using-google-fonts-in-a-tailwind-project/

--- Saturday Nights/Cursor Follower (Tailwind CSS)/tailwind.config.js
+++ Saturday Nights/Cursor Follower (Tailwind CSS)/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'press-start': ['"Press Start 2P"', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
