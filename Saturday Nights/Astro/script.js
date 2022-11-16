09c691a656366f4d8e19118e364e9d16e10ed144
cemalpay <cem.alpay.tas@common.studio>
2022-11-16 17:51:20 +0300

    Create cursor follow function

--- Saturday Nights/Astro/script.js
+++ Saturday Nights/Astro/script.js
@@ -7,7 +7,7 @@ function createStars(type, quantity) {
         star.classList.add('star', `type-${type}`);
         star.style.left = `${randomNumber(1, 99)}%`;
         star.style.bottom = `${randomNumber(-5, 99)}%`;
-        star.style.animationDuration = `${randomNumber(500, 900)}s`;
+        star.style.animationDuration = `${randomNumber(300, 900)}s`;
         document.body.appendChild(star);
     }
 }
@@ -20,3 +20,33 @@ createStars(1, 80);
 createStars(2, 80);
 createStars(3, 80);
 
+//Cursor follower function
+
+const cursor = document.querySelector('.cursor')
+
+let mouseX = 0
+let mouseY = 0
+
+window.addEventListener('mousemove', (event) => {
+    mouseY = (event.clientY / 16) - (45 / 16) + 'rem'
+    mouseX = (event.clientX / 16) - (45 / 16) + 'rem'
+} )
+
+const mouseMove = () => {
+    cursor.style.top = mouseY
+    cursor.style.left = mouseX
+
+    window.requestAnimationFrame(mouseMove)
+}
+mouseMove()
+
+//Title Split & Join for z-index
+
+const labels = document.querySelectorAll('.label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText 
+    .split('')
+    .map((letter, idx) => `<span style="z-index:${idx }">${letter}</span>`)
+    .join('')
+})
