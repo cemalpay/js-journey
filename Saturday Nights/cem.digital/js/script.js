a5db7cde7ddcbdb807062c011e7d541cb143c4a1
cemalpay <cem.alpay.tas@common.studio>
2022-11-26 16:23:02 +0300

    Add variables

--- Saturday Nights/cem.digital/js/script.js
+++ Saturday Nights/cem.digital/js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const webDev = document.querySelector('.btn-web') 
+const uxUi = document.querySelector('.btn-uxui')
+const design = document.querySelector('.btn-design')
+const info = document.querySelector('.btn-info')
+const contact = document.querySelector('.btn-contact')
+
+
