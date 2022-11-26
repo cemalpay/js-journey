44344893836a031801b30561a0322870aecd321f
cemalpay <cem.alpay.tas@common.studio>
2022-11-26 17:19:17 +0300

    Add buttons hover function with forEach loop

--- Saturday Nights/cem.digital/js/script.js
+++ Saturday Nights/cem.digital/js/script.js
@@ -1,9 +1,32 @@
 'use strict';
 
-const webDev = document.querySelector('.btn-web') 
-const uxUi = document.querySelector('.btn-uxui')
-const design = document.querySelector('.btn-design')
-const info = document.querySelector('.btn-info')
-const contact = document.querySelector('.btn-contact')
+const webDev = document.getElementById('btn-web') 
+const uxUi = document.getElementById('btn-uxui')
+const design = document.getElementById('btn-design')
+const info = document.getElementById('btn-info')
+const contact = document.getElementById('btn-contact')
+const twitter = document.getElementById('btn-twitter')
+const github = document.getElementById('btn-github')
 
+var menuItems = [
+    webDev, 
+    uxUi, 
+    design,
+    info, 
+    contact,
+    twitter,
+    github
+]
 
+menuItems.forEach(hover)
+function hover(item, index, arr) {
+    menuItems[index]
+
+menuItems[index].addEventListener('mouseover', () => {
+    menuItems[index].style.color = "#fff"
+} )
+menuItems[index].addEventListener('mouseout', () => {
+    menuItems[index].style.color = ""
+} )
+
+}
