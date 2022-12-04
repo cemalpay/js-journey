5b9754efa8cf364f1ba3e4ecd9eb091945ad5e8c
cemalpay <cem.alpay.tas@common.studio>
2022-12-04 18:02:56 +0300

    Update index.js
    Add gravity to player

--- Saturday Nights/Tiny Platform (Browser Game)/index.js
+++ Saturday Nights/Tiny Platform (Browser Game)/index.js
@@ -4,5 +4,29 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle="white"
-c.fillRect(0, 0, canvas.width, canvas.height)
+class Player {
+    constructor(position) {
+        this.position = position
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 100, 100)
+    }
+    update() {
+        this.draw()
+        this.position.y++
+    }
+}
+const player = new Player({
+    x:0,
+    y:0,
+})
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle="white"
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+}
+
+animate()
