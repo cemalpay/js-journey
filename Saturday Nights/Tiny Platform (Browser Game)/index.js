9627a5ee6086184fa175c15fe438c0a563af5d03
cemalpay <cem.alpay.tas@common.studio>
2022-12-04 18:11:47 +0300

    Add if statement for gravity

--- Saturday Nights/Tiny Platform (Browser Game)/index.js
+++ Saturday Nights/Tiny Platform (Browser Game)/index.js
@@ -12,15 +12,18 @@ class Player {
             x:0,
             y:1,
         }
+        this.height = 100
     }
     draw() {
         c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 100, 100)
+        c.fillRect(this.position.x, this.position.y, 100, this.height)
     }
     update() {
         this.draw()
         this.position.y += this.velocity.y
+        if (this.position.y + this.height + this.velocity.y< canvas.height)
         this.velocity.y += gravity
+        else this.velocity.y = 0
     }
 }
 const player = new Player({
