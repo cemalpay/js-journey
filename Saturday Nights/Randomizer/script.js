c2619a386885f78111cf4f5555f8a16daab00868
cemalpay <cem.alpay.tas@common.studio>
2022-12-06 21:24:42 +0300

    Add if/else statement

--- Saturday Nights/Randomizer/script.js
+++ Saturday Nights/Randomizer/script.js
@@ -9,4 +9,11 @@ function printRandomSelection() {
     setTimeout(function() {
         result.innerHTML = selection;
       }, 500); // 0.5 saniye bekleme
+    if (selection === 'red') {
+    result.style.color = 'red'; // h1'in rengini kırmızı olarak ayarla
+    } else if (selection === 'black') {
+    result.style.color = 'black'; // h1'in rengini siyah olarak ayarla
+    } else if (selection === 'green') {
+        result.style.color = 'green'
+    }
 }
