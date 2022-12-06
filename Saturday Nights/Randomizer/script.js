41e5366ccf84d82ed9a186aa3ccdd88c59a87b0d
cemalpay <cem.alpay.tas@common.studio>
2022-12-06 20:50:33 +0300

    -

--- Saturday Nights/Randomizer/script.js
+++ Saturday Nights/Randomizer/script.js
@@ -1,1 +1,5 @@
 'use strict';
+function printRandomSelection() {
+    // Create an array with the red and black choices
+    var choices = ['green','red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black'];
+    var selection = choices[Math.floor(Math.random() * choices.length)];
