a3ba16eb4c24cb577f0c2bfb8996764a1fbdad73
cemalpay <cem.alpay.tas@common.studio>
2022-12-06 21:01:45 +0300

    add some styling

--- Saturday Nights/Randomizer/script.js
+++ Saturday Nights/Randomizer/script.js
@@ -1,5 +1,8 @@
 'use strict';
 function printRandomSelection() {
     // Create an array with the red and black choices
-    var choices = ['green','red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black'];
+    var choices = ['red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black','green'];
     var selection = choices[Math.floor(Math.random() * choices.length)];
+    document.getElementById("result").innerHTML = selection;
+
+}
