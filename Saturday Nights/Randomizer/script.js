04c041c283e28419e7914331230382403e711dd5
cemalpay <cem.alpay.tas@common.studio>
2022-12-06 21:20:28 +0300

    Add setTimeout function

--- Saturday Nights/Randomizer/script.js
+++ Saturday Nights/Randomizer/script.js
@@ -1,8 +1,12 @@
 'use strict';
+const result = document.getElementById("result")
+
 function printRandomSelection() {
     // Create an array with the red and black choices
     var choices = ['red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black','green'];
-    var selection = choices[Math.floor(Math.random() * choices.length)];
-    document.getElementById("result").innerHTML = selection;
-
+    const selection = choices[Math.floor(Math.random() * choices.length)];
+    result.innerHTML = "-";
+    setTimeout(function() {
+        result.innerHTML = selection;
+      }, 500); // 0.5 saniye bekleme
 }
