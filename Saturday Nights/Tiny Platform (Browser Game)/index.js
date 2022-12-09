3f1067af8d227d34b9725eb6dfb4fb8833342c52
cemalpay <cem.alpay.tas@common.studio>
2022-12-09 18:19:55 +0300

    -

--- Saturday Nights/Tiny Platform (Browser Game)/index.js
+++ Saturday Nights/Tiny Platform (Browser Game)/index.js
@@ -7,8 +7,8 @@ canvas.width = 1024
 canvas.height = 576
 
 const scaledCanvas = {
-    width: canvas.width / 4,
-    height: canvas.height / 4,
+    width: canvas.width ,
+    height: canvas.height ,
 }
 //yerçekimi çarpanı
 const gravity = 0.3
@@ -99,11 +99,11 @@ function animate() {
     //save ve restore arasına alınan fonksiyonlar bir kere çalışıyor
     c.save()
     //arkaplanı 4 kat büyüttük
-    // c.scale(4, 4)
+    //c.scale(2, 2)
 
     // //arkaplanın pozisyonunu ayarladık (x:0 y:(-bg.height) + (canvas.height / 4))
-    // c.translate(0,-background.image.height + scaledCanvas.height)
-    // background.update()
+    c.translate(0,-background.image.height + scaledCanvas.height)
+    background.update()
 
     //map yüklendiğinde collision blockların yüklenmesini sağlıyoruz.
     //
--- Saturday Nights/Tiny Platform (Browser Game)/js/classes/CollisionBlock.js
+++ Saturday Nights/Tiny Platform (Browser Game)/js/classes/CollisionBlock.js
@@ -5,7 +5,7 @@ class CollisionBlock {
         this.height = 16
     }
     draw(){
-        c.fillStyle = 'rgba(255,0,0,0.5)'
+        c.fillStyle = 'rgba(255,0,0,0)'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
     update() {
