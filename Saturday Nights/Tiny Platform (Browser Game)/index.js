0612609f6d794f1a1af4f74e6b07a9fac4663628
cemalpay <cem.alpay.tas@common.studio>
2022-12-10 02:59:19 +0300

    Clear bugs

--- Saturday Nights/Tiny Platform (Browser Game)/index.js
+++ Saturday Nights/Tiny Platform (Browser Game)/index.js
@@ -10,21 +10,15 @@ const scaledCanvas = {
     width: canvas.width / 4,
     height: canvas.height / 4,
 }
-//yerçekimi çarpanı
-const gravity = 0.1
+
 
 // 36 satır x 27 sütun
 // her block 16x16
 // her 36lık satırı bölüp 2d arrayine pushladık
 const floorCollisions2D = []
 for (let i = 0; i < floorCollisions.length; i += 36) {
-    floorCollisions2D.push(floorCollisions.slice(i, i + 36))
+  floorCollisions2D.push(floorCollisions.slice(i, i + 36))
 }
-const platformCollisions2D = []
-for (let i = 0; i < platformCollisions.length; i += 36) {
-    platformCollisions2D.push(platformCollisions.slice(i, i + 36))
-}
-
 /*
 rowlardaki değerler bizim y deki pozisyonumuzu veriyor
 column(symbol)'deki değerler ise x deki pozisyonumuzu.
@@ -34,90 +28,97 @@ orada bir collision block oluşturulmasını sağlayacağız
 */
 const collisionBlocks = []
 floorCollisions2D.forEach((row, y) => {
-    row.forEach((symbol, x) => {
-        if(symbol === 199) {
-            console.log('draw a block here!')
-            collisionBlocks.push(new CollisionBlock({
-                position: {
-                    x: x * 16,
-                    y: y * 16,
-                }
-            }))
-        }
-    })
-})
-const platformCollisionBlocks = []
-platformCollisions2D.forEach((row, y) => {
-    row.forEach((symbol, x) => {
-        if(symbol === 199) {
-            console.log('draw a block here!')
-            //!!! değiştirmeyi unutma!!! geçici olarak platform collisionları floor ile aynı yaptık
-            platformCollisionBlocks.push(new CollisionBlock({
-                position: {
-                    x: x * 16,
-                    y: y * 16,
-                },
-                height: 4,
-            }))
-        }
-    })
+  row.forEach((symbol, x) => {
+    if (symbol === 199) {
+      collisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 16,
+            y: y * 16,
+          },
+        })
+      )
+    }
+  })
 })
 
+const platformCollisions2D = []
+for (let i = 0; i < platformCollisions.length; i += 36) {
+  platformCollisions2D.push(platformCollisions.slice(i, i + 36))
+}
+const platformCollisionBlocks = []
+platformCollisions2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 199) {
+      platformCollisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 16,
+            y: y * 16,
+          },
+          height: 4,
+        })
+      )
+    }
+  })
+})
+
+//yerçekimi çarpanı
+const gravity = 0.1
 
 // new player yaratıldı ve başlangıç pozisyonu belirlendi
 const player = new Player({
-    position:{
-        x:100,
-        y:100
+    position: {
+      x: 40,
+      y: 350,
     },
     collisionBlocks,
     platformCollisionBlocks,
-    imageSrc:'./img/warrior/Idle.png',
+    imageSrc: './img/warrior/Idle.png',
     frameRate: 8,
     animations: {
-        Idle: {
-            imageSrc: './img/warrior/Idle.png',
-            frameRate: 8,
-            frameBuffer: 3,
-        },
-        Run: {
-            imageSrc: './img/warrior/Run.png',
-            frameRate: 8,
-            frameBuffer: 5,
-        },
-        Jump: {
-            imageSrc: './img/warrior/Jump.png',
-            frameRate: 2,
-            frameBuffer: 3,
-          },
-          Fall: {
-            imageSrc: './img/warrior/Fall.png',
-            frameRate: 2,
-            frameBuffer: 3,
-          },
-          FallLeft: {
-            imageSrc: './img/warrior/FallLeft.png',
-            frameRate: 2,
-            frameBuffer: 3,
-          },
-          RunLeft: {
-            imageSrc: './img/warrior/RunLeft.png',
-            frameRate: 8,
-            frameBuffer: 5,
-          },
-          IdleLeft: {
-            imageSrc: './img/warrior/IdleLeft.png',
-            frameRate: 8,
-            frameBuffer: 3,
-          },
-          JumpLeft: {
-            imageSrc: './img/warrior/JumpLeft.png',
-            frameRate: 2,
-            frameBuffer: 3,
-          },
-    }
-})
-
+      Idle: {
+        imageSrc: './img/warrior/Idle.png',
+        frameRate: 8,
+        frameBuffer: 3,
+      },
+      Run: {
+        imageSrc: './img/warrior/Run.png',
+        frameRate: 8,
+        frameBuffer: 5,
+      },
+      Jump: {
+        imageSrc: './img/warrior/Jump.png',
+        frameRate: 2,
+        frameBuffer: 3,
+      },
+      Fall: {
+        imageSrc: './img/warrior/Fall.png',
+        frameRate: 2,
+        frameBuffer: 3,
+      },
+      FallLeft: {
+        imageSrc: './img/warrior/FallLeft.png',
+        frameRate: 2,
+        frameBuffer: 3,
+      },
+      RunLeft: {
+        imageSrc: './img/warrior/RunLeft.png',
+        frameRate: 8,
+        frameBuffer: 5,
+      },
+      IdleLeft: {
+        imageSrc: './img/warrior/IdleLeft.png',
+        frameRate: 8,
+        frameBuffer: 3,
+      },
+      JumpLeft: {
+        imageSrc: './img/warrior/JumpLeft.png',
+        frameRate: 2,
+        frameBuffer: 3,
+      },
+    },
+  })
 // keys adında sağ ve sol yön tuşlarının pressed durumunu belirten bir array
 const keys = {
     d: {
@@ -146,64 +147,54 @@ const camera = {
 
 function animate() {
     window.requestAnimationFrame(animate)
-    c.fillStyle="white"
+    c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
-    c.translate(camera.position.x, camera.position.y)
-
-    //save ve restore arasına alınan fonksiyonlar bir kere çalışıyor
+  
     c.save()
-    //arkaplanı 4 kat büyüttük
     c.scale(4, 4)
-
-    // //arkaplanın pozisyonunu ayarladık (x:0 y:(-bg.height) + (canvas.height / 4))
-    // c.translate(0,-background.image.height + scaledCanvas.height)
+    c.translate(camera.position.x, camera.position.y)
     background.update()
-
-    //map yüklendiğinde collision blockların yüklenmesini sağlıyoruz.
-    //
-    //restore satırının üstünde olmalı çünkü scale ediliyor harita
-    // collisionBlocks.forEach((CollisionBlock) => {
-    //     CollisionBlock.update()
+    // collisionBlocks.forEach((collisionBlock) => {
+    //   collisionBlock.update()
     // })
+  
     // platformCollisionBlocks.forEach((block) => {
-    //     block.update()
+    //   block.update()
     // })
+  
     player.checkForHorizontalCanvasCollision()
-    //player'in x düzlemindeki hareketi
     player.update()
-
+  
     player.velocity.x = 0
     if (keys.d.pressed) {
-        player.switchSprite('Run')
-        player.velocity.x = 2
-        player.lastDirection = 'right'
-        player.shouldPanCameraToTheLeft({ canvas, camera })
+      player.switchSprite('Run')
+      player.velocity.x = 2
+      player.lastDirection = 'right'
+      player.shouldPanCameraToTheLeft({ canvas, camera })
     } else if (keys.a.pressed) {
-        player.switchSprite('RunLeft')
-        player.velocity.x = -2
-        player.lastDirection = 'left'
-        player.shouldPanCameraToTheRight({ canvas, camera })
+      player.switchSprite('RunLeft')
+      player.velocity.x = -2
+      player.lastDirection = 'left'
+      player.shouldPanCameraToTheRight({ canvas, camera })
     } else if (player.velocity.y === 0) {
-        if (player.lastDirection === 'right') player.switchSprite('Idle')
-        else player.switchSprite('IdleLeft')
+      if (player.lastDirection === 'right') player.switchSprite('Idle')
+      else player.switchSprite('IdleLeft')
     }
-
+  
     if (player.velocity.y < 0) {
-        player.shouldPanCameraDown({ camera, canvas })
-        if (player.lastDirection === 'right') player.switchSprite('Jump')
-        else player.switchSprite('JumpLeft')
+      player.shouldPanCameraDown({ camera, canvas })
+      if (player.lastDirection === 'right') player.switchSprite('Jump')
+      else player.switchSprite('JumpLeft')
     } else if (player.velocity.y > 0) {
-        player.shouldPanCameraUp({ camera, canvas })
-        if (player.lastDirection === 'right') player.switchSprite('Fall')
-        else player.switchSprite('FallLeft')
+      player.shouldPanCameraUp({ camera, canvas })
+      if (player.lastDirection === 'right') player.switchSprite('Fall')
+      else player.switchSprite('FallLeft')
     }
-
-
+  
     c.restore()
-
-}
-
-animate()
+  }
+  
+  animate()
 
 //arrow keys'in pressed true olma durumu
 window.addEventListener('keydown', (event) => {
