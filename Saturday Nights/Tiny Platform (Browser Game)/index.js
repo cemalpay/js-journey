cc1a67cf97988bf11ae0b73a9e07d17b27e85c6e
cemalpay <cem.alpay.tas@common.studio>
2022-12-10 02:41:03 +0300

    Add Jump, Fall & Left versions

--- Saturday Nights/Tiny Platform (Browser Game)/index.js
+++ Saturday Nights/Tiny Platform (Browser Game)/index.js
@@ -85,6 +85,36 @@ const player = new Player({
             frameRate: 8,
             frameBuffer: 5,
         },
+        Jump: {
+            imageSrc: './img/warrior/Jump.png',
+            frameRate: 2,
+            frameBuffer: 3,
+          },
+          Fall: {
+            imageSrc: './img/warrior/Fall.png',
+            frameRate: 2,
+            frameBuffer: 3,
+          },
+          FallLeft: {
+            imageSrc: './img/warrior/FallLeft.png',
+            frameRate: 2,
+            frameBuffer: 3,
+          },
+          RunLeft: {
+            imageSrc: './img/warrior/RunLeft.png',
+            frameRate: 8,
+            frameBuffer: 5,
+          },
+          IdleLeft: {
+            imageSrc: './img/warrior/IdleLeft.png',
+            frameRate: 8,
+            frameBuffer: 3,
+          },
+          JumpLeft: {
+            imageSrc: './img/warrior/JumpLeft.png',
+            frameRate: 2,
+            frameBuffer: 3,
+          },
     }
 })
 
