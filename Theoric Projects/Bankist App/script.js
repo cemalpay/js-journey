fd0a5f3f17fbd227879fe5d8185a4fda059b8d0e
cemalpay <cem.alpay.tas@common.studio>
2022-12-13 18:19:56 +0300

    Create movements.map for usd to euro

--- Theoric Projects/Bankist App/script.js
+++ Theoric Projects/Bankist App/script.js
@@ -91,5 +91,10 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+//kur 1.1 gibi euroyu 1.1 ile çarpıp dolar cinsinden gösteriyoruz.
+const euroToUsd = 1.1;
+const movementsUSD = movements.map(function (mov) {
+  return mov * euroToUsd;
+})
+console.log(movementsUSD);
+console.log(movements);
