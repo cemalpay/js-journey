74dd80df4349f3cd1bba47c4e7e84ac9fe95c92f
cemalpay <cem.alpay.tas@common.studio>
2022-12-13 16:25:51 +0300

    Update script.js

--- Theoric Projects/Bankist App/script.js
+++ Theoric Projects/Bankist App/script.js
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Cem Alpay Tas',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Payidar Kaya',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Recep Tayyip Erdogan',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
-  pin: 3333,
+  pin: 2023,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Ekrem Imamoglu',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
