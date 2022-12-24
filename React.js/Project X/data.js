ee3028948846b2953eb0b49610301bd00e7e478b
cemalpay <cem.alpay.tas@common.studio>
2022-12-24 17:08:01 +0300

    Create starting files

---
+++ React.js/Project X/data.js
@@ -0,0 +1,48 @@
+const CATEGORIES = [
+  { name: "wordpress", color: "#3b82f6" },
+  { name: "github", color: "#16a34a" },
+  { name: "vscode", color: "#ef4444" },
+  { name: "javascript", color: "#eab308" },
+  { name: "google cloud", color: "#db2777" },
+  { name: "docker", color: "#14b8a6" },
+  { name: "general", color: "#f97316" },
+  { name: "starters", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "general",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "general",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "github",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+// LINK TO APP SAMPLE DATA: https://docs.google.com/spreadsheets/d/1eeldcA_OwP4DHYEvjG0kDe0cRys-cDPhc_E9P9G1e3I/edit#gid=0
+
+// 👍 🤯 ⛔️
