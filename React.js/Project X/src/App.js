f142a57dc8311e3ad9b9d048ac7d4d9ff0e77023
cemalpay <cem.alpay.tas@common.studio>
2022-12-29 03:15:19 +0300

    first contact with JSX

--- React.js/Project X/src/App.js
+++ React.js/Project X/src/App.js
@@ -1,5 +1,15 @@
+import "./css/style.css";
+import "./css/style.css.map";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <header class="header">
+      <div class="logo">
+        <img src="logo.png" alt="" />
+      </div>
+      <button class="btn btn-large btn-open">Create new X</button>
+    </header>
+  );
 }
 
 export default App;
