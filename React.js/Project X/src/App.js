6ffa0b3dad5a0e511781f149c12b6b8394bcf86c
cemalpay <cem.alpay.tas@common.studio>
2022-12-30 03:34:47 +0300

    rendering the list of x's

--- React.js/Project X/src/App.js
+++ React.js/Project X/src/App.js
@@ -1,15 +1,49 @@
 import "./css/style.css";
 import "./css/style.css.map";
 
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "general",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "general",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "github",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
 function App() {
   return (
     <>
       {/*HEADER*/}
-      <header class="header">
-        <div class="logo">
+      <header className="header">
+        <div className="logo">
           <img src="logo.png" alt="" />
         </div>
-        <button class="btn btn-large btn-open">Create new X</button>
+        <button className="btn btn-large btn-open">Create new X</button>
       </header>
       <NewXForm />
       {/*MAIN*/}
@@ -27,7 +61,26 @@ function CategoryFilter() {
   return <aside>Category Filter</aside>;
 }
 function XList() {
-  return <section>X List</section>;
+  const Xs = initialFacts;
+  return (
+    <section>
+      <ul className="x-list">
+        {Xs.map((X) => (
+          <li key={Xs.id} className="x-text">
+            {X.text}
+            <a className="source" href={X.source} target="_blank">
+              (Source)
+            </a>
+            <span className="tag">{X.category}</span>
+            <div className="vote-buttons">
+              <button className="btn-vote">⛔ 9</button>
+              <button className="btn-vote">🦄 4</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default App;
