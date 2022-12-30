fa6504d49ffef6cb1b82dc5948a8c4209eefe473
cemalpay <cem.alpay.tas@common.studio>
2022-12-30 03:17:09 +0300

    divide interface into components

--- React.js/Project X/src/App.js
+++ React.js/Project X/src/App.js
@@ -3,13 +3,31 @@ import "./css/style.css.map";
 
 function App() {
   return (
-    <header class="header">
-      <div class="logo">
-        <img src="logo.png" alt="" />
-      </div>
-      <button class="btn btn-large btn-open">Create new X</button>
-    </header>
+    <>
+      {/*HEADER*/}
+      <header class="header">
+        <div class="logo">
+          <img src="logo.png" alt="" />
+        </div>
+        <button class="btn btn-large btn-open">Create new X</button>
+      </header>
+      <NewXForm />
+      {/*MAIN*/}
+      <main className="main">
+        <CategoryFilter />
+        <XList />
+      </main>
+    </>
   );
+}
+function NewXForm() {
+  return <form className="x-form">New X Form</form>;
+}
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+function XList() {
+  return <section>X List</section>;
 }
 
 export default App;
