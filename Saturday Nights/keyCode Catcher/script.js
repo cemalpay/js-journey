cfd84d02ad17ad5153fb2944c1e2529766887d3d
cemalpay <cem.alpay.tas@common.studio>
2023-01-18 01:22:25 +0300

    js

--- Saturday Nights/keyCode Catcher/script.js
+++ Saturday Nights/keyCode Catcher/script.js
@@ -1,1 +1,18 @@
-'use strict';
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', function (event) {
+  insert.innerHTML = `
+
+    <div class="key">
+      ${event.key === ' ' ? 'Space' : event.key}
+      <small> event.key </small>
+    </div>
+    <div class="key">
+      ${event.keyCode}
+      <small> event.keyCode </small>
+    </div>
+    <div class="key">
+      ${event.code}
+      <small> event.code </small>
+    </div>`;
+});
