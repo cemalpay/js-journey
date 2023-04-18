99c5106e2915e88128d4fd529f07794b9ff05e08
cemalpay <cem.alpay.tas@common.studio>
2023-04-18 18:32:19 +0300

    create glitch effect

--- Saturday Nights/Cyberpunk Card/script.js
+++ Saturday Nights/Cyberpunk Card/script.js
@@ -1,1 +1,31 @@
-'use strict';
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let interval = null;
+
+const screen = document.querySelector('.screen'),
+  heading = document.querySelector('.heading');
+
+screen.onmouseenter = event => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    heading.innerText = heading.innerText
+      .split('')
+      .map((letter, index) => {
+        if (index < iteration) {
+          return heading.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join('');
+
+    if (iteration >= heading.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+};
