da46d55d945e344c6b4f2f71180356eef649b95a
cemalpay <cem.alpay.tas@common.studio>
2023-06-26 22:43:09 +0300

    -

--- React.js/Eat-N-Split/eat-n-split/src/App.js
+++ React.js/Eat-N-Split/eat-n-split/src/App.js
@@ -1,20 +1,34 @@
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Berfin",
     image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
+    balance: 50,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Cem",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Gayrullah",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  return <ul> List</ul>;
+}
