400c4158c3a9e8696a356f06de2ad79b57c6d935
cemalpay <cem.alpay.tas@common.studio>
2023-06-27 02:14:21 +0300

    create FormAddFriend()

--- React.js/Eat-N-Split/eat-n-split/src/App.js
+++ React.js/Eat-N-Split/eat-n-split/src/App.js
@@ -24,6 +24,8 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
     </div>
   );
@@ -60,7 +62,25 @@ function Friend({ friend }) {
       {friend.balance === 0 && (
         <p className="blue">You and {friend.name} are even</p>
       )}
-      <button className="button">Settle up</button>
+      <Button>Settle Up</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>Friend Name</label>
+      <input type="text" />
+
+      <label>Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
   );
 }
