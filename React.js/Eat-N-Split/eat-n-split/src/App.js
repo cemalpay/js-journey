d768a88bb589f9464b40bbe0bc01c99298125799
cemalpay <cem.alpay.tas@common.studio>
2023-06-27 02:24:33 +0300

    create FormSplitBill()

--- React.js/Eat-N-Split/eat-n-split/src/App.js
+++ React.js/Eat-N-Split/eat-n-split/src/App.js
@@ -27,6 +27,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -81,6 +82,30 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>Bill Value</label>
+      <input type="text" />
+
+      <label>Your Expense</label>
+      <input type="text" />
+
+      <label>X's expense</label>
+      <input type="text" disabled />
+
+      <label>Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split Bill</Button>
     </form>
   );
 }
