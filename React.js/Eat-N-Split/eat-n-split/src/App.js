951e3f33cecaf0e40c5ae3f5966b6264c646b68a
cemalpay <cem.alpay.tas@common.studio>
2023-06-27 17:43:53 +0300

    create form conditions

--- React.js/Eat-N-Split/eat-n-split/src/App.js
+++ React.js/Eat-N-Split/eat-n-split/src/App.js
@@ -88,18 +88,24 @@ function Friend({ friend }) {
 
 function FormAddFriend() {
   const [name, setName] = useState("");
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
 
   function handleSubmit(e) {
     e.preventDefault();
 
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
     const newFriend = {
+      id,
       name,
-      image,
+      image: `${image}?=${id}`,
       balance: 0,
-      id: crypto.randomUUID,
     };
     console.log(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48");
   }
   return (
     <form className="form-add-friend" onSubmit={handleSubmit}>
