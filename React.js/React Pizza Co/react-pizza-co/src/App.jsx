c0b839f35a199d8b4ccb394261e45cc0fcbe8efa
cemalpay <cem.alpay.tas@common.studio>
2023-07-04 01:48:53 +0300

    add new routes

--- React.js/React Pizza Co/react-pizza-co/src/App.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/App.jsx
@@ -2,6 +2,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +14,18 @@ const router = createBrowserRouter([
   {
     path: "/menu",
     element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
   },
 ]);
 
