78f79fa36d8bdda4d638ab63bd1bf6a6a25b02c1
cemalpay <cem.alpay.tas@common.studio>
2023-07-04 17:29:54 +0300

    create useLoaderData()
    getMenu from api

--- React.js/React Pizza Co/react-pizza-co/src/features/menu/Menu.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/features/menu/Menu.jsx
@@ -1,5 +1,13 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+
 function Menu() {
+  const menu = useLoaderData();
   return <h1>Menu</h1>;
 }
 
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
 export default Menu;
