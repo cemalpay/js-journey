a4636e67529218220953bd8c896ec30ccab1ea78
cemalpay <cem.alpay.tas@common.studio>
2023-07-05 00:08:42 +0300

    import { Form } *-*
    react-router-dom  bizim için stateleri falan hallediyo.
    sadece bizim createOrderAction'u router'a eklememiz gerekiyor.

--- React.js/React Pizza Co/react-pizza-co/src/features/order/CreateOrder.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { Form } from "react-router-dom";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -38,7 +38,7 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -72,9 +72,16 @@ function CreateOrder() {
         <div>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  return null;
 }
 
 export default CreateOrder;
