067a618ffb6f3c1532e56c86dd81b1cf6b75bc78
cemalpay <cem.alpay.tas@common.studio>
2023-07-05 00:54:12 +0300

    error handling

--- React.js/React Pizza Co/react-pizza-co/src/features/order/CreateOrder.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -32,6 +32,11 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigate = useNavigate();
+  const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -50,6 +55,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -72,7 +78,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing order..." : "Order Now!"}
+          </button>
         </div>
       </Form>
     </div>
@@ -92,6 +100,14 @@ export async function action({ request }) {
   console.log(order);
 
   const newOrder = await createOrder(order);
+
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone = "Invalid phone number";
+  }
+  if (Object.keys(errors).length > 0) {
+    return errors;
+  }
 
   return redirect(`/order/${newOrder.id}`);
 }
