63d3df45909e3291c83abcb3d7f241110b81adbd
cemalpay <cem.alpay.tas@common.studio>
2023-07-04 17:53:53 +0300

    get pizza menu from api
    menu.map(pizza)

--- React.js/React Pizza Co/react-pizza-co/src/features/menu/Menu.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/features/menu/Menu.jsx
@@ -1,9 +1,16 @@
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
-  return <h1>Menu</h1>;
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
