a0d9e4b57b768417083f11305c716f013e3c2057
cemalpay <cem.alpay.tas@common.studio>
2023-07-04 16:53:30 +0300

    create AppLayout.jsx

---
+++ React.js/React Pizza Co/react-pizza-co/src/ui/AppLayout.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Content</h1>
+      </main>
+    </div>
+  );
+}
+
+export default AppLayout;
---
+++ React.js/React Pizza Co/react-pizza-co/src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <header>
+      <Link to="/">Home</Link>
+      <p>Cem</p>
+    </header>
+  );
+}
+
+export default Header;
