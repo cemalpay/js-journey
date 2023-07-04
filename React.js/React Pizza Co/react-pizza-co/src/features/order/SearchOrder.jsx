d6ecf8020daec3ccadfbede6ec85f18c5454533c
cemalpay <cem.alpay.tas@common.studio>
2023-07-04 23:40:48 +0300

    fetch orders

---
+++ React.js/React Pizza Co/react-pizza-co/src/features/order/SearchOrder.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search Order"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;
