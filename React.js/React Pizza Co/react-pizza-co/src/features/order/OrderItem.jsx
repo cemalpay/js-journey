c69d0fd01361391705d3bac6f5ddf4a46c915086
cemalpay <cem.alpay.tas@common.studio>
2023-07-24 00:55:12 +0300

    update Order Status page

--- React.js/React Pizza Co/react-pizza-co/src/features/order/OrderItem.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/features/order/OrderItem.jsx
@@ -1,13 +1,13 @@
-import { formatCurrency } from "utils";
+import { formatCurrency } from "../../utils/helpers";
 
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
-      <div>
+    <li className="py-3 ">
+      <div className="flex items-center justify-between gap-4 text-sm">
         <p>
-          <span>{quantity}&times;</span> {name}
+          <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p>{formatCurrency(totalPrice)}</p>
       </div>
