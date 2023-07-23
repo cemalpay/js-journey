df2fff48063f136aee946d91c3714543b578f9b4
cemalpay <cem.alpay.tas@common.studio>
2023-07-23 17:30:03 +0300

    update create order button with new prop focus:

--- React.js/React Pizza Co/react-pizza-co/src/features/order/CreateOrder.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/features/order/CreateOrder.jsx
@@ -78,7 +78,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300  hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2"
+          >
             {isSubmitting ? "Placing order..." : "Order Now!"}
           </button>
         </div>
