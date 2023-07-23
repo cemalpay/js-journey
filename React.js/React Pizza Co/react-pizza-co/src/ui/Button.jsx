bb2e977514cc24df1eabe6ad4c94211bc32f2bc8
cemalpay <cem.alpay.tas@common.studio>
2023-07-23 23:05:58 +0300

    type prop for Button.jsx

--- React.js/React Pizza Co/react-pizza-co/src/ui/Button.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/ui/Button.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
-function Button({ children, disabled, to }) {
-  const className =
-    "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300  hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-5 sm:py-4";
+function Button({ children, disabled, to, type }) {
+  const base =
+    "inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300  hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+
+  const styles = {
+    primary: base + "px-4 py-3 md:px-5 md:py-4",
+    small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
+  };
 
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
