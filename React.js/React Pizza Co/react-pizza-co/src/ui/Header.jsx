ad708cc0a66467d71692c8f17aa0ba162a6e4a37
cemalpay <cem.alpay.tas@common.studio>
2023-07-23 19:34:32 +0300

    add custom font via tailwind.config.js
    *overwrite sans

--- React.js/React Pizza Co/react-pizza-co/src/ui/Header.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from "../features/order/SearchOrder";
 import Username from "../features/user/Username";
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 font-semibold uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         React Pizza Co.
       </Link>
