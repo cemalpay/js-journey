8be6325fc3d89bde5e8f86d081325752930fe6d1
cemalpay <cem.alpay.tas@common.studio>
2023-07-24 00:20:43 +0300

    update New Order page

--- React.js/React Pizza Co/react-pizza-co/src/ui/Button.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/ui/Button.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 function Button({ children, disabled, to, type }) {
   const base =
-    "inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300  hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+    "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300  hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
 
   const styles = {
     primary: base + "px-4 py-3 md:px-5 md:py-4",
     small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
     secondary:
-      "inline-block rounded-full font-semibold border-2 border-stone-300 uppercase tracking-wide text-stone-800 transition-colors duration-300  hover:bg-stone-800 hover:text-white focus:bg-stone-800 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-5 md:py-3.5",
+      "inline-block text-sm rounded-full font-semibold border-2 border-stone-300 uppercase tracking-wide text-stone-800 transition-colors duration-300  hover:bg-stone-800 hover:text-white focus:bg-stone-800 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-5 md:py-3.5",
   };
 
   if (to)
