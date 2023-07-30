ef24a67bbb3f46ac50c3e0f3f5f88d28e1e78dd5
cemalpay <cem.alpay.tas@common.studio>
2023-07-31 01:36:52 +0300

    Modeling the "User" State With Redux Toolkit
    create userSlice.js file
    create store.js file on src folder
    add provider to main.jsx for provide stored information
    import useSelector and collect data from store.js on username.jsx file

--- React.js/React Pizza Co/react-pizza-co/src/features/user/userSlice.js
+++ React.js/React Pizza Co/react-pizza-co/src/features/user/userSlice.js
@@ -26,6 +26,22 @@ async function fetchAddress() {
 }
 */
 
+const { createSlice } = require("@reduxjs/toolkit");
+
 const initialState = {
   username: "",
 };
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
--- React.js/React Pizza Co/react-pizza-co/src/main.jsx
+++ React.js/React Pizza Co/react-pizza-co/src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
