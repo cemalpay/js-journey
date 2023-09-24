455dd9db08697462d69baa724a2256a9b56a4bc7
cemalpay <cem.alpay.tas@common.studio>
2023-09-25 00:35:28 +0300

    import framer

--- React.js/Demos/01 — Landing Page/circle-landing/src/App.jsx
+++ React.js/Demos/01 — Landing Page/circle-landing/src/App.jsx
@@ -1,25 +1,54 @@
-import { useState } from 'react'
-
-import './App.css'
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const content = [
+    {
+      title: 'Cappadocia',
+      url: 'src/images/cappadocia.jpg',
+    },
+    {
+      title: 'Ephesus',
+      url: 'src/images/ephesus.jpg',
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0); // Başlangıçta ilk öğeyi göster
+
+  const handleNext = async () => {
+    // Resmi değiştir
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % content.length);
+  };
+
+  const currentContent = content[currentIndex];
 
   return (
     <>
-      <div className="App" >
-        <h1>Cappadocia</h1>
-        <div className="imageCenter" style={{
-        backgroundImage: `url("src/images/cappadocia.jpg")`,
-      }}></div>
-        <div className="imageRing" style={{
-        backgroundImage: `url("src/images/cappadocia.jpg")`,
-      }}></div>
-        <div className="imageOuter" style={{
-        backgroundImage: `url("src/images/cappadocia.jpg")`,
-      }}></div>
+      <div className="App">
+        <motion.div
+          className="imageCenter"
+          style={{
+            backgroundImage: `url(${currentContent.url})`,
+          }}
+          initial={{ rotate: 0 }}
+          animate={{ rotate: 360 }}
+          transition={{ duration: 1, repeat: null}}
+        ></motion.div>
+        <motion.div className="imageRing" style={{ backgroundImage: `url(${currentContent.url})` }}></motion.div>
+        <motion.div className="imageOuter" style={{ backgroundImage: `url(${currentContent.url})` }}></motion.div>
+        <div className="container">
+          <h1>{currentContent.title}</h1>
+          <div className='buttons'>
+          <button className="btn btn_next" onClick={handleNext}>
+            Next
+          </button>
+          <button className="btn btn_prev">Prev</button>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
